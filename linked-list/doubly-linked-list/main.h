6598a87180d18888fe66d22c8401c8bee5cca177
rungene <rungenesamuel@gmail.com>
2023-01-30 07:46:41 +0000

    Doubly Linked List - Implementation, insert the node at the begging and printing in forward as well as reverse

--- linked-list/doubly-linked-list/main.h
+++ linked-list/doubly-linked-list/main.h
@@ -1,4 +1,4 @@
-i#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -7,11 +7,11 @@ i#include <stdio.h>
 * @next: member of pointer to the next node
 * @prev: member of pointer to the previous node
 *
-* Description: singly linked list node structure
+* Description: doublly linked list node structure
 */
 typedef struct node
 {
 	int data;
-	struct node *prev
+	struct node *prev;
 	struct node *next;
 }node;
